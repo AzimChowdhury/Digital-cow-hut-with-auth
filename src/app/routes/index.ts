2c637b4563537ef90a1072659a10fe57f8876ed6
Azim Chowdhury <azimchy994@gmail.com>
2023-06-17 01:28:00 +0800

    done

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { UserRoutes } from "../modules/users/user.route";
 import { signupRoutes } from "../modules/Signup/signup.route";
 const router = express.Router();
-
+// build
 const moduleRoutes = [
   {
     path: "/auth",
